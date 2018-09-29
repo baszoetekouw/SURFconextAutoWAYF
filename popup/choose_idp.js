a4f13b9cb89dcd809e5ff6cd219cfbe67ccdc819
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2018-09-29 12:14:46 +0200

    remove background page, noo need for it

--- popup/choose_idp.js
+++ popup/choose_idp.js
@@ -1,23 +1,8 @@
 
-function handleError(error) {
-	console.log("CS: handleError: ");
-	console.log(error);
-}
-
-function handleResponse(message) {
-	console.log("CS: handleResponse: ");
-	console.log(message);
-}
-
-function notifyBackgroundPage(msg) {
-	console.log("CS: sending message");
-	console.log(msg);
-	var sending = browser.runtime.sendMessage(msg);
-	sending.then(handleResponse, handleError);
-}
-
-function storeSettings(idp) {
-	browser.storage.local.set({ idp: idp });
+function storeSettings(settings) {
+	console.log("Storing settings");
+	console.log(settings);
+	browser.storage.local.set(settings);
 }
 
 function setDefault(idp)
@@ -43,7 +28,7 @@ function listenForClicks()
 		function selected_idp(idp) {
 			console.log("CS: Selected IdP "+idp);
 			storeSettings({ idp: idp });
-			notifyBackgroundPage({type: "select_idp", idp: idp});
+			//notifyBackgroundPage({type: "select_idp", idp: idp});
 		}
 
 		function reset_idp() {
